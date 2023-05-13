c40285329ef235d27b56ebcda38c048ed1b86a97
Sumit Kumar Chaurasiya <94757093+sumit9919003012@users.noreply.github.com>
2023-05-13 14:51:22 +0530

    Update config.py
     the possibility to execute commands in a Telegram chat, which then directly trigger actions or request information.
    For example, it is possible to send the bot the command “/help” or “/help”, which then outputs the commands possible for this bot in the chat as text feedback.

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "" # 🤖 Get It From My.telegram.org.
     BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    BOT_HELP = "" # 🤖 Get It Help From @GroupHelpBot (Telegram) By Sending /help  Command. 
